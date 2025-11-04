683678e678985357bd67759f0ca56fdcabc6c40a
matdombrock <dombrock.mathieu@gmail.com>
2025-11-03 18:40:05 -0800

    mobile

--- res/example.js
+++ res/example.js
@@ -42,3 +42,27 @@ async function runDemo() {
   asa.printClassNames();
 }
 runDemo();
+
+const showPlaylistListButton = document.getElementById('asa-show-playlists');
+const playlistListContainer = document.getElementById('asa-playlist-list-container');
+const playerContainer = document.getElementById('asa-player-container');
+if (!showPlaylistListButton) {
+  throw new Error('Show playlist list button not found');
+}
+if (!playlistListContainer) {
+  throw new Error('Playlist list container not found');
+}
+if (!playerContainer) {
+  throw new Error('Player container not found');
+}
+showPlaylistListButton.addEventListener('click', async () => {
+  console.log('Showing playlist list container');
+  if (playerContainer.style.width === '100%') {
+    playlistListContainer.style.width = '100%';
+    playerContainer.style.width = '0';
+  }
+  else {
+    playlistListContainer.style.width = '0';
+    playerContainer.style.width = '100%';
+  }
+});
